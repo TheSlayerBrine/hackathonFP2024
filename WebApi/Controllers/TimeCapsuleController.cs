5abbe2b3ffc9571c27396340285143f4c13b32d8
Costin Cazacu <costincazacu2015@gmail.com>
2024-04-27 17:22:30 +0300

    122

--- WebApi/Controllers/TimeCapsuleController.cs
+++ WebApi/Controllers/TimeCapsuleController.cs
@@ -1,6 +1,25 @@
-﻿namespace WebApi.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Services.TimeCapsules;
+using WebApi.Mappers;
+using WebApi.Models.TimeCapsules;
 
-public class TimeCapsuleController
+namespace WebApi.Controllers;
+[ApiController]
+[Route("api/timecapsules")]
+public class TimeCapsuleController : BaseController
 {
-    
+    private readonly ITimeCapsuleService timeCapsuleService;
+
+    public TimeCapsuleController(ITimeCapsuleService timeCapsuleService)
+    {
+        this.timeCapsuleService = timeCapsuleService;
+    }
+
+    [HttpPost("createTimeCapsule")]
+    public ActionResult<TimeCapsuleModel> CreateTimeCapsule(CreateTimeCapsuleModel model)
+    {
+        var timeCapsule = new TimeCapsuleMapper().CreateTimeCapsuleModelToCreateTimeCapsuleDto(model);
+        timeCapsuleService.CreateTimeCapsule(timeCapsule);
+        return Ok();
+    }
 }
---
+++ WebApi/Mappers/TimeCapsuleMapper.cs
@@ -0,0 +1,11 @@
+﻿using Riok.Mapperly.Abstractions;
+using Service.Services.TimeCapsules;
+using WebApi.Models.TimeCapsules;
+
+namespace WebApi.Mappers;
+
+[Mapper]
+public partial class TimeCapsuleMapper
+{
+    public partial CreateTimeCapsuleDto CreateTimeCapsuleModelToCreateTimeCapsuleDto(CreateTimeCapsuleModel model);
+}
