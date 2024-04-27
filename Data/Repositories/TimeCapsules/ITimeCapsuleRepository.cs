d0b520f8d34a3603d0e44e3b4c63cb441b6120d0
Costin Cazacu <costincazacu2015@gmail.com>
2024-04-27 14:43:42 +0300

    ..

---
+++ Data/Repositories/TimeCapsules/ITimeCapsuleRepository.cs
@@ -0,0 +1,10 @@
+﻿using Data.Entities;
+using Data.Infrastructure.Repository;
+
+namespace Data.Repositories.TimeCapsules;
+
+public interface ITimeCapsuleRepository : IRepository<TimeCapsule>
+{
+    public TimeCapsule GetById(int id);
+    public TimeCapsule GetByName(string name);
+}
---
+++ Data/Repositories/TimeCapsules/TimeCapsuleRepository.cs
@@ -0,0 +1,43 @@
+﻿using Data.Entities;
+using Data.Infrastructure.Context;
+using Data.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories.TimeCapsules;
+
+public class TimeCapsuleRepository : Repository<TimeCapsule>, ITimeCapsuleRepository
+{
+    private readonly IAppDbContext dbContext;
+    
+    public TimeCapsuleRepository(IAppDbContext dbContext) : base((AppDbContext)dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    
+    public void Add(TimeCapsule entity)
+    {
+        dbContext.TimeCapsules.Add(entity);
+    }
+    public void Delete(TimeCapsule entity)
+    {
+        dbContext.TimeCapsules.Remove(entity);
+    }
+    public TimeCapsule Find(int id)
+    {
+        return dbContext.TimeCapsules.Where(x => x.Id == id).FirstOrDefault();
+    }
+    public TimeCapsule GetById(int id)
+    {
+        var TimeCapsule = dbContext.TimeCapsules.FirstOrDefault(x => x.Id == id);
+        return TimeCapsule;
+    }
+    public IEnumerable<TimeCapsule> GetAll()
+    {
+        return dbContext.TimeCapsules.ToList();
+    }
+
+    public TimeCapsule GetByName(string name)
+    {
+        return dbContext.TimeCapsules.Where(x => x.Title == name).FirstOrDefault();
+    }
+}
